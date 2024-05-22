4433d71e781c020963c2c0f7abf8f53d16a4e6c0
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-05-22 09:41:47 +0000

    initialised multidimensional arrays

---
+++ MultidimensionalArr.java
@@ -0,0 +1,12 @@
+public class MultidimensionalArr{
+    public static void main(Strin[] args){
+
+        int[][] arr = new int[3][];
+
+        int[][] arr = {
+            {1,2,3},
+            {4,5,6}.
+            {7,8,9}
+        }
+    }
+}
