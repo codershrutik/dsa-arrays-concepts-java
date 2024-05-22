63c1d41e62fcd4946f490db897b136dc4638bdd7
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-05-22 09:52:54 +0000

    user input and output

--- MultidimensionalArr.java
+++ MultidimensionalArr.java
@@ -8,5 +8,24 @@ public class MultidimensionalArr{
             {4,5,6}.
             {7,8,9}
         }
+
+        // input
+        for (int row = 0; row < arr.length; row++) {
+
+            for (int col = 0; col < arr[row].length; col++) {
+
+                arr[row][col] = sc.nextInt();
+            }
+        }
+
+        //output
+        for (int row = 0; row < arr.length; row++) {
+
+            for (int col = 0; col < arr[row].length; col++) {
+
+                System.out.print(arr[row][col] + " ");
+            }
+        }
+        
     }
 }
